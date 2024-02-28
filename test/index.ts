f3aac3631e7fb6ac3814ac675734b8e3dd396b7e
tamaina <tamaina@hotmail.co.jp>
2024-02-28 22:56:09 +0000

    move keys.ts

--- test/index.ts
+++
@@ -1,9 +0,0 @@
-import { dirname } from 'node:path';
-import { fileURLToPath } from 'node:url';
-import { describe } from '@jest/globals';
-
-const _filename = fileURLToPath(import.meta.url);
-const _dirname = dirname(_filename);
-
-describe('test', () => {
-});
