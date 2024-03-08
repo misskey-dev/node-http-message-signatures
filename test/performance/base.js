35c2e847fd16bfdf081459b06a1eda964f304bcf
tamaina <tamaina@hotmail.co.jp>
2024-03-08 15:33:53 +0000

    chalk

--- test/performance/base.js
+++ test/performance/base.js
@@ -1,3 +1,5 @@
+import chalk from "chalk";
+
 function round3(value) {
 	const pow = Math.pow(10, 3);
 	return Math.round(value * pow) / pow;
@@ -6,15 +8,17 @@ function round3(value) {
 export class PerformanceBase {
 	constructor(
 		TRYES,
+		log = console.log,
 	) {
 		this.TRYES = TRYES;
-		console.log('Performance test, TRYES:', TRYES);
+		this.log = log;
+		this.log(chalk.bold('Performance test, TRYES:'), TRYES);
 	}
 
 	start(
 		name,
 	) {
-		console.log(name);
+		this.log(chalk.underline(name));
 		this.name = name;
 		this.startTime = process.hrtime();
 	}
@@ -28,6 +32,9 @@ export class PerformanceBase {
 		this.name = undefined;
 		this.startTime = undefined;
 		const ms = diff[0] * 1e3 + diff[1] / 1e6;
-		console.log(round3(ms), 'ms', round3(this.TRYES / (ms / 1e3)), 'ops/s');
+		this.log(
+			chalk.gray(chalk.yellow(round3(ms).toString().padStart(14))), 'ms',
+			chalk.gray(chalk.yellow(round3(this.TRYES / (ms / 1e3)).toString().padStart(14))), 'ops/s',
+		);
 	}
 }
