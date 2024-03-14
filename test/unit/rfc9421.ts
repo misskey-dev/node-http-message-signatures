67f6a6f6fe9c5aa6872436f684b639e70aa710d4
tamaina <tamaina@hotmail.co.jp>
2024-03-15 01:44:46 +0900

    modify test

--- test/unit/rfc9421.ts
+++ test/unit/rfc9421.ts
@@ -1,4 +1,4 @@
-import { HeadersLike } from "@/types";
+import { HeadersLike, RequestLike } from "@/types";
 import { RFC9421SignSource, processSingleRFC9421SignSource, signAsRFC9421ToRequestOrResponse } from "@/rfc9421/sign";
 import { rsa4096 } from "test/keys";
 import { getMap } from "@/utils";
@@ -85,29 +85,42 @@ describe('RFC9421', () => {
 		});
 	});
 
-	describe('integrated', () => {
+	const basicExpectedSignature = 'gGI1l9aRpu7oejIDYbTP9oskjnS5rZbWScELpUl2dU80Ct63PFVsAWxJSa102qo3ykUvLf/OgHnmX5qXkrDeG0DXh0DP9m6sD6KzW3sgqDMBzSGMy8sjallfqtZT8u/N0U97/Mkck0GaTUxXNVDjR2iPcIjaA3c04w1Wp4y7Ww8qffRifelX2AUbFut4Pil60GaU1EFs3mDQEBIoOp+T50KQEKnkQpktzU0DshBori2j4gVEQw2hIH/G+gfzTBdUT8o6tvU6FSqGRWJakCOSFImqRLJcCc7NsX1je7O6D3mO8Mtp3tZdVjIYKZn/A45dA9xB0LEnXMQUN/TTIy7VG3uGfKGNV8k6ivvqQwcOf9FPoPmmIo+ejd48/+yosRYB2YdvVzN0h3Y5LiPT58ZKpze4MKxr7LeEcyzkXBd4eo9RK3q6MDRdrSxNRXDLXUQ7GI41aiJY+ginc4ZKpYUPdmFNf2H7HY4NMSPQCt8c0XNYMohA4znKLH4YaqtAUOO2YqupwIHtAzXriG/yCs3l5NvXnS8hXnYd4BM68t/gdxtJy9N66gZilXzDqh6fM7ptLKYHzUpvOfH6uWud3qgJJWcY21iSFfsZ48STrNIoF4/lWYrG4vQAPrDvuq6qH3sOMUpkRzQuITUIa9DqEBebkZdKTGgN30147dV65z4Qwc0=';
+	const basicSources = {
+		sig1: {
+			key: {
+				privateKeyPem: rsa4096.privateKey,
+				keyId: 'x',
+			},
+			defaults: {
+				ec: 'DSA',
+				hash: 'SHA-256',
+			},
+			identifiers: basicIdentifiers,
+			created: 1618884475,
+		},
+	} satisfies Record<string, RFC9421SignSource>;
+
+	describe(signAsRFC9421ToRequestOrResponse, () => {
 		test('basic', async () => {
 			const request = getBasicRequest();
-			const expectedSignature = 'gGI1l9aRpu7oejIDYbTP9oskjnS5rZbWScELpUl2dU80Ct63PFVsAWxJSa102qo3ykUvLf/OgHnmX5qXkrDeG0DXh0DP9m6sD6KzW3sgqDMBzSGMy8sjallfqtZT8u/N0U97/Mkck0GaTUxXNVDjR2iPcIjaA3c04w1Wp4y7Ww8qffRifelX2AUbFut4Pil60GaU1EFs3mDQEBIoOp+T50KQEKnkQpktzU0DshBori2j4gVEQw2hIH/G+gfzTBdUT8o6tvU6FSqGRWJakCOSFImqRLJcCc7NsX1je7O6D3mO8Mtp3tZdVjIYKZn/A45dA9xB0LEnXMQUN/TTIy7VG3uGfKGNV8k6ivvqQwcOf9FPoPmmIo+ejd48/+yosRYB2YdvVzN0h3Y5LiPT58ZKpze4MKxr7LeEcyzkXBd4eo9RK3q6MDRdrSxNRXDLXUQ7GI41aiJY+ginc4ZKpYUPdmFNf2H7HY4NMSPQCt8c0XNYMohA4znKLH4YaqtAUOO2YqupwIHtAzXriG/yCs3l5NvXnS8hXnYd4BM68t/gdxtJy9N66gZilXzDqh6fM7ptLKYHzUpvOfH6uWud3qgJJWcY21iSFfsZ48STrNIoF4/lWYrG4vQAPrDvuq6qH3sOMUpkRzQuITUIa9DqEBebkZdKTGgN30147dV65z4Qwc0=';
-			const sources = {
-				sig1: {
-					key: {
-						privateKeyPem: rsa4096.privateKey,
-						keyId: 'x',
-					},
-					defaults: {
-						ec: 'DSA',
-						hash: 'SHA-256',
-					},
-					identifiers: basicIdentifiers,
-					created: 1618884475,
-				},
-			} satisfies Record<string, RFC9421SignSource>;
-			const result = await signAsRFC9421ToRequestOrResponse(request, getMap(sources));
+			await signAsRFC9421ToRequestOrResponse(request, getMap(basicSources));
 			expect(request.headers['Signature-Input']).toBe('sig1=("@method" "@authority" "@path" "@query" "content-digest" "accept");keyid="x";alg="rsa-v1_5-sha256";created=1618884475');
-			expect(request.headers['Signature']).toBe(`sig1=:${expectedSignature}:`);
+			expect(request.headers['Signature']).toBe(`sig1=:${basicExpectedSignature}:`);
+		});
+	});
 
-			expect(() => parseRFC9421RequestOrResponse(request)).toThrow(); // Invalid skew
+	describe('integrated', () => {
+		let request: RequestLike;
+		let result: Awaited<ReturnType<typeof signAsRFC9421ToRequestOrResponse>>;
+		beforeAll(async () => {
+			request = getBasicRequest();
+			result = await signAsRFC9421ToRequestOrResponse(request, getMap(basicSources));
+		});
+		test('parse (invalid skew fail)', () => {
+			expect(() => parseRFC9421RequestOrResponse(request)).toThrow();
+		});
+		test('parse and verify', async () => {
 			const parsed = parseRFC9421RequestOrResponse(request, { clockSkew: { now: theDate } });
 			expect(parsed.value).toStrictEqual([
 				[
@@ -120,13 +133,37 @@ describe('RFC9421', () => {
 						keyid: 'x',
 						nonce: undefined,
 						params: '("@method" "@authority" "@path" "@query" "content-digest" "accept");keyid="x";alg="rsa-v1_5-sha256";created=1618884475',
-						signature: expectedSignature,
+						signature: basicExpectedSignature,
 						tag: undefined,
 					},
 				],
 			]);
 			const verified = await verifyParsedSignature(parsed, rsa4096.publicKey);
 			expect(verified).toBe(true);
+		});
+
+		test('parse (invalid signature verify false)', async () => {
+			const invalidSignature = `${Array.from({ length: 86 }, () => 'A').join('')}==`;
+			request.headers['Signature'] = `sig1=:${invalidSignature}:`;
+			const parsed = parseRFC9421RequestOrResponse(request, { clockSkew: { now: theDate } });
+			expect(parsed.value).toStrictEqual([
+				[
+					'sig1',
+					{
+						algorithm: 'rsa-v1_5-sha256',
+						base: result.signatureBases.get('sig1'),
+						created: 1618884475,
+						expires: undefined,
+						keyid: 'x',
+						nonce: undefined,
+						params: '("@method" "@authority" "@path" "@query" "content-digest" "accept");keyid="x";alg="rsa-v1_5-sha256";created=1618884475',
+						signature: invalidSignature,
+						tag: undefined,
+					},
+				],
+			]);
+			const verified = await verifyParsedSignature(parsed, rsa4096.publicKey);
+			expect(verified).toBe(false);
 		});
 	});
 });
